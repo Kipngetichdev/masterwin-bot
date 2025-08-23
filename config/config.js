809bd2ed3aa4c1d2376df1384232cf545c8a0784
Kipngetichdev <digitaltechsolutionz24.7@gmail.com>
2025-08-24 02:05:39 +0300

    test live app

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   botToken: process.env.BOT_TOKEN || '8368433706:AAFFSWTk0kUUoRXsR4_UQrWPlNSZn9WG7A0',
   webhookPath: '/api/telegram-bot',
-  appUrl: process.env.VERCEL_URL || 'https://your-vercel-app.vercel.app',
+  appUrl: process.env.VERCEL_URL || 'https://masterwin-bot.vercel.app/',
   subscriptionUrl: 'https://masterwin-app.com/subscribe' // Update with actual URL
 };
